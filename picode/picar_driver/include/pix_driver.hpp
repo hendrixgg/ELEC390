888e1378113c8d04852e10ce51691fce74d9688e
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-02-26 18:38:10 -0500

    create prj

---
+++ picode/picar_driver/include/pix_driver.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file pix_driver.hpp
+ * @author Jacob Chisholm (https://jchisholm.github.io)
+ * @brief PiCarX Driver Header File
+ * @date 2025-02-26
+ * @version 0.1
+ *
+ */
+
+#ifndef _PIX_DRIVER_H_
+#define _PIX_DRIVER_H_
+
+
+#endif
---
+++ picode/picar_driver/src/pix_driver.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file pix_driver.cpp
+ * @author Jacob Chisholm (https://jchisholm.github.io)
+ * @brief PiCarX Driver
+ * @date 2025-02-26
+ * @version 0.1
+ *
+ */
+
+#include "pix_driver.hpp"
