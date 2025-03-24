a69660e450e3b0fa77e515f6b4d764bfc9a1fb50
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-03-23 20:09:32 -0400

    jetson launch

---
+++ ros2_ws/launch/jetson_launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            name='realsense',
+            parameters=[
+                # {"enable_depth": True},
+                # {"enable_color": True},
+                # {"enable_infra": True},
+                {"publish_tf": False},  # Disable transform publishing
+                # {"enable_gyro": False},  # Disable IMU (gyro)
+                {"enable_accel": True},  # Disable IMU (accelerometer)
+                {"enable_sync": True},  # Sync depth and color frames
+                {"depth_module.depth_profile": "640x480x30"},
+                {"rgb_camera.color_profile": "1280x720x30"},
+                {"depth_module.infra_profile": "640x480x30"},
+                ],
+        ),
+    ])
