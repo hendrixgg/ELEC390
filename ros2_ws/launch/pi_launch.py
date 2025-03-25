7165ad61e6cace36f29e75be9cf358379158d7b0
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-03-24 22:23:45 -0400

    testing

--- ros2_ws/launch/pi_launch.py
+++ ros2_ws/launch/pi_launch.py
@@ -15,25 +15,25 @@ def generate_launch_description():
             name='pix_control',
             parameters=[{"turn_offset": 1.0, "diff_ratio": 0.65}]
             ),
-        Node(
-            package='driver',
-            executable='driver',
-            name='controller',
-            parameters=[{"drive_power": 40.0,
-                         "left_turn_angle": -20.0,
-                         "pid_p": 0.1,
-                         "pid_i": 0.2,
-                         "pid_d": 0.05,
-                         "intersection_time_ms": 300,
-                         }]
-            ),
-        Node(
-            package='vision',
-            executable='roadline_tracker',
-            name='roadline_tracker',
-            parameters=[{"image_topic": "/pix_camera/image_raw",
-                         "output_topic": "line_deviation"}]
-            ),
+        # Node(
+        #     package='driver',
+        #     executable='driver',
+        #     name='controller',
+        #     parameters=[{"drive_power": 40.0,
+        #                  "left_turn_angle": -20.0,
+        #                  "pid_p": 0.1,
+        #                  "pid_i": 0.2,
+        #                  "pid_d": 0.05,
+        #                  "intersection_time_ms": 300,
+        #                  }]
+        #     ),
+        # Node(
+        #     package='vision',
+        #     executable='roadline_tracker',
+        #     name='roadline_tracker',
+        #     parameters=[{"image_topic": "/pix_camera/image_raw",
+        #                  "output_topic": "line_deviation"}]
+        #     ),
         # Node(
         #     package='realsense2_camera',
         #     executable='realsense2_camera_node',
